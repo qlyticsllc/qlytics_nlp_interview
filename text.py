1849b3204ecc605db8f68dad95f6b6dc2b666d94
ZijianHe <zijianhe.dev@gmail.com>
2018-11-12 12:34:07 -0500

    finished task 4

--- text.py
+++ text.py
@@ -2,7 +2,10 @@ input_text = "Total capital assets (net)                        $      4,191,448
 
 
 def process(text):
-    return []
+    entries = [entry.strip() for entry in text.split('$')]
+    for idx, entry in enumerate(entries[1:]):
+        entries[idx + 1] = '${}'.format(entry)
+    return entries
 
 
 assert process(input_text) == ['Total capital assets (net)', '$4,191,448', '$4,044,748', '$137,724', '$146,304', '$4,329,172', '$4,191,052']
