8ebff355871b898e01bc68e84d47c3c527806f0b
ZhangDa <fengzhucanhua@163.com>
2018-11-13 15:16:53 +0800

    Add code to make the script run.

--- text.py
+++ text.py
@@ -1,8 +1,13 @@
 input_text = "Total capital assets (net)                        $      4,191,448   $   4,044,748   $     137,724     $   146,304     $   4,329,172   $   4,191,052"
 
-
 def process(text):
-    return []
-
+    result = []
+    phrases = text.split('$')
+    for i in range(0,len(phrases)):
+        if i == 0:
+            result.append(phrases[i].strip(' '))
+        else:
+            result.append("$"+phrases[i].strip(' '))
+    return result
 
 assert process(input_text) == ['Total capital assets (net)', '$4,191,448', '$4,044,748', '$137,724', '$146,304', '$4,329,172', '$4,191,052']
