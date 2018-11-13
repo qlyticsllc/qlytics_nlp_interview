6e84126b90eefbdb52199d115ed8cd7e69e019a2
plumbn <nate.plumb@gmail.com>
2018-11-13 16:00:24 +0800

    task 3

--- simplify.py
+++ simplify.py
@@ -1,14 +1,12 @@
+import string
+
 text = "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way-in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only."
 
 
 def is_valid(text, portion):
-    sequence = ''
-    for c in text:
-        sequence += c
-        if len(sequence) == len(portion):
-            if sequence == portion:
-                return True
-            sequence = sequence[1:]
+    sequence = text[:len(portion)]
+    if sequence == portion:
+        return True
     return False
 
 
@@ -33,11 +31,10 @@ def capture_text():
         for c in entry:
             if is_valid(sequence, ' of '):
                 sequence = ''
-                if c not in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
+                if c not in string.ascii_uppercase:
                     capture = True
             sequence += c
         if (capture and
             sequence not in captured):
             filtered.append(sequence)
     return filtered
-
