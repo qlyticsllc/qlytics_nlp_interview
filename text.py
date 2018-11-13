a740a2d1f251cf9f9cbb02302fa51c8d63093afa
POFK <maotianxiang@bao.ac.cn>
2018-11-13 23:57:51 +0800

    Task 4

--- text.py
+++ text.py
@@ -2,7 +2,15 @@ input_text = "Total capital assets (net)                        $      4,191,448
 
 
 def process(text):
-    return []
+    n = 0
+    capture = []
+    for w in text.split('$'):
+        if n == 0:
+            capture.append(w.strip())
+        else:
+            capture.append("$" + w.strip())
+        n += 1 
+    return capture
 
 
 assert process(input_text) == ['Total capital assets (net)', '$4,191,448', '$4,044,748', '$137,724', '$146,304', '$4,329,172', '$4,191,052']
