87548acf2d8562d51d7831181d8c9f015c7ee2d7
Chris Lee <mouzhecc@gmail.com>
2018-11-15 15:28:57 +0800

    text

--- text.py
+++ text.py
@@ -2,7 +2,14 @@ input_text = "Total capital assets (net)                        $      4,191,448
 
 
 def process(text):
-    return []
+    words = [
+        word.strip() if i == 0 else '$' + word.strip()
+        for i, word in enumerate(text.split('$'))
+    ]
+    return words
 
 
-assert process(input_text) == ['Total capital assets (net)', '$4,191,448', '$4,044,748', '$137,724', '$146,304', '$4,329,172', '$4,191,052']
+assert process(input_text) == [
+    'Total capital assets (net)', '$4,191,448', '$4,044,748', '$137,724',
+    '$146,304', '$4,329,172', '$4,191,052'
+]
