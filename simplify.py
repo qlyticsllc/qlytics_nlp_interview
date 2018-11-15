27eda534b1c17a587fc6170e1bcca1242a4fcf7a
Chris Lee <mouzhecc@gmail.com>
2018-11-15 15:21:50 +0800

    simplify

--- simplify.py
+++ simplify.py
@@ -1,43 +1,22 @@
 text = "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way-in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only."
 
-
-def is_valid(text, portion):
-    sequence = ''
-    for c in text:
-        sequence += c
-        if len(sequence) == len(portion):
-            if sequence == portion:
-                return True
-            sequence = sequence[1:]
-    return False
+# What the script does is find those clause with "it was the "(case insensitive).
+# And find the words after the " of ".
+# At last filter out those With capitals.
+# So I rewrite them all from scratch.
 
 
 def capture_text():
-    sequence = ''
-    capture = False
-    captured = []
-    for c in text:
-        sequence += c
-        if (capture and
-            sequence.endswith(', ')):
-            capture = False
-            captured.append(sequence[:-2])
-            sequence = ''
-        if is_valid(sequence.lower(), 'it was the '):
-            sequence = ''
-            capture = True
+    clauses = text.split(',')
     filtered = []
-    for entry in captured:
-        sequence = ''
-        capture = False
-        for c in entry:
-            if is_valid(sequence, ' of '):
-                sequence = ''
-                if c not in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
-                    capture = True
-            sequence += c
-        if (capture and
-            sequence not in captured):
-            filtered.append(sequence)
+    for clause in clauses:
+        if clause.lower().find('it was the') != -1:
+            word = clause[len('it was the '):]
+            word = word[word.find(' of ') + len(' of '):]
+            if not word.istitle():
+                filtered.append(word)
     return filtered
 
+
+if __name__ == "__main__":
+    print(capture_text())
