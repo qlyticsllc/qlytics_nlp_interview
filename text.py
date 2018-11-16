ce40fe0b9360b6d4ea840507524a69a0ecca7f81
Michael(Zhiyi Weng) <vyloy.w@gmail.com>
2018-11-16 10:52:04 +0800

    Task 4. Python programming: text processing

--- text.py
+++ text.py
@@ -2,7 +2,15 @@ input_text = "Total capital assets (net)                        $      4,191,448
 
 
 def process(text):
-    return []
+    splits = text.split('$')
+    result = []
+    for i, s in enumerate(splits):
+        s = s.strip()
+        if i == 0:
+            result.append(s)
+        else:
+            result.append('$'+s)
+    return result
 
 
 assert process(input_text) == ['Total capital assets (net)', '$4,191,448', '$4,044,748', '$137,724', '$146,304', '$4,329,172', '$4,191,052']
